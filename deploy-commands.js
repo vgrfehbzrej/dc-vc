534bce153d775d644226692d191313e2a988bfe4
vgrfehbzrej <162656983+vgrfehbzrej@users.noreply.github.com>
2026-02-21 07:37:28 +0100

    Update application and server IDs in deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -16,7 +16,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
     // Trage hier deine Application ID & Server ID ein
     await rest.put(
-      Routes.applicationGuildCommands('DEINE_APPLICATION_ID', 'DEINE_GUILD_ID'),
+      Routes.applicationGuildCommands('1474629086042394664', '1427681480741748828'),
       { body: commands }
     );
 
